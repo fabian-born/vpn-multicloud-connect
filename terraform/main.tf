2a50e09af1d8f8a1660757022d32d7c0534887c6
Fabian Born <fabian.born@fabianborn.net>
2019-08-30 09:31:02 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,8 @@
+
 provider "azurerm" {
+  version = "=1.31.0"
+  tenant_id = "4b0911a0-929b-4715-944b-c03745165b3a"
+  subscription_id = "04e10b5f-7248-43c5-a56c-7fce5c485f28"
 }
 
 provider "google" {
